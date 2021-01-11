2d170db560d161e2e1ec186658cd4d214f08f9a7
Lucas Passos <lucasplpx@gmail.com>
2021-01-11 16:09:58 -0300

    Customização do tema

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { ThemeProvider } from '@material-ui/core/styles';
 import Home from './pages/Home';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Home />
-    </div>
+    </ThemeProvider>
   );
 }
 
