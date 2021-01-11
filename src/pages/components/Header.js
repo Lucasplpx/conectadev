a075eb6aaea9ef66ff866cc2ad877ebbd996c277
Lucas Passos Lima <lucasplpx@gmail.com>
2021-01-11 10:20:18 -0300

    Initial Code

---
+++ src/pages/components/Header.js
@@ -0,0 +1,20 @@
+function components() {
+  return (
+    <header className="header">
+      <div className="toolbar">
+        <div>
+          <a href="/">Conecta Dev</a>
+        </div>
+
+        <div>
+          <button>Novo Post</button>
+
+          <span>img1</span>
+          <span>img2</span>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default components;
