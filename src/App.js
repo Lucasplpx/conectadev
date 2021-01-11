91a0f7ecb251e2ad848264804564a02a9513aeaf
Lucas Passos <lucasplpx@gmail.com>
2021-01-11 17:09:54 -0300

    Configurando as rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import SignIn from './pages/SignIn';
 import Home from './pages/Home';
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="*" element={<h1>Not found 404 ;(</h1>} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
