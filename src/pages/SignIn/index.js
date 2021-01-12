381f56ce4938d5a7622e910c487b4ff2cc56c49c
Lucas Passos <lucasplpx@gmail.com>
2021-01-12 09:23:46 -0300

    Estilizando a page SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,38 +1,64 @@
-import { Typography } from '@material-ui/core';
+import { Avatar, Button, Link, TextField, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
+import { Grid, Box } from '@material-ui/core';
+import { LockOutlined } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
     height: '100vh',
   },
-  left: {
-    background: 'blue',
-    flexBasis: '58%',
 
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+  image: {
+    backgroundImage: 'url(/images/background.jpg)',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'none',
+    padding: theme.spacing(2),
+    textAlign: 'center',
   },
-  righ: {
-    background: 'yellow',
-    flexBasis: '42%',
+  avatar: {
+    background: theme.palette.primary.main,
+    marginBottom: theme.spacing(1),
+  },
+  button: {
+    marginTop: theme.spacing(1),
   },
   form: {
-    display: 'flex',
-    flexDirection: 'column',
-    margin: '64px 32px',
-    alignItems: 'center',
+    margin: theme.spacing(2, 4),
   },
-});
+}));
+
+function Copyright() {
+  return (
+    <Typography variant="body2" align="center">
+      {'Copyright © '}
+      <a
+        color="inherit"
+        target="_blanck"
+        href="https://github.com/Lucasplpx/conectadev"
+      >
+        Lucas Passos
+      </a>{' '}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
 
 function SignIn() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <div className={classes.left}>
+    <Grid container className={classes.root}>
+      <Grid
+        className={classes.image}
+        md={7}
+        item
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
         <Typography style={{ color: '#FFF', fontSize: 35, lineHeight: '45px' }}>
           <strong>
             Simplificando a forma de conectar desenvolvedores de software!
@@ -51,17 +77,61 @@ function SignIn() {
             de software.
           </strong>
         </Typography>
-      </div>
+      </Grid>
 
-      <div className={classes.righ}>
-        <form className={classes.form}>
-          <h4>Acesso</h4>
+      <Grid md={5} item>
+        <Box display="flex" flexDirection="column" alignItems="center" mt={8}>
+          <Avatar className={classes.avatar}>
+            <LockOutlined />
+          </Avatar>
+          <Typography variant="h5">Acesso</Typography>
 
-          <input type="text" />
-          <input type="text" />
-        </form>
-      </div>
-    </div>
+          <form className={classes.form}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="E-mail"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              label="Senha"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+            />
+
+            <Button
+              className={classes.button}
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Entrar
+            </Button>
+
+            <Grid container>
+              <Grid item>
+                <Link>Esqueceu sua senha?</Link>
+              </Grid>
+              <Grid item>
+                <Link>Não tem um conta? Registra-se</Link>
+              </Grid>
+            </Grid>
+          </form>
+          <Copyright />
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
